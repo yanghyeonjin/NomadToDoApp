554d5be7253fcee4c44716ce6ea6fa0f0e504a01
yanghyeonjin <guswls7081@gmail.com>
2025-01-06 18:29:50 +0900

    AsyncStorage를 사용하여 To Do 데이터를 저장하고 유지하기

--- App.tsx
+++ App.tsx
@@ -7,30 +7,58 @@ import {
   TextInput,
   ScrollView,
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { theme } from './colors';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type ToDo = {
   text: string;
-  work: boolean;
+  working: boolean;
 };
+type ToDoList = {
+  [key: string]: ToDo;
+};
+
+const STORAGE_KEY = '@toDos';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState('');
-  const [toDos, setToDos] = useState<{ [key: string]: ToDo }>({});
+  const [toDos, setToDos] = useState<ToDoList>({});
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
 
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload: string) => setText(payload);
-  const addTodo = () => {
+  const saveToDos = async (toSave: ToDoList) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+    } catch (e) {
+      // saving error
+    }
+  };
+  const loadToDos = async () => {
+    try {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+      if (s !== null) {
+        setToDos(JSON.parse(s));
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+  const addToDo = async () => {
     if (text === '') return;
 
     // save to do
-    const newTodos = Object.assign({}, toDos, {
-      [Date.now()]: { text, work: working },
+    const newToDos: ToDoList = Object.assign({}, toDos, {
+      [Date.now()]: { text, working },
     });
-    setToDos(newTodos);
+    setToDos(newToDos);
+    await saveToDos(newToDos);
 
     // clear input
     setText('');
@@ -62,7 +90,7 @@ export default function App() {
 
       <TextInput
         onChangeText={onChangeText}
-        onSubmitEditing={addTodo}
+        onSubmitEditing={addToDo}
         style={styles.input}
         placeholder={working ? 'Add a To do' : 'Where do you want to go?'}
         value={text}
@@ -70,11 +98,13 @@ export default function App() {
       />
 
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
-          <View key={key} style={styles.toDo}>
-            <Text style={styles.todoText}>{toDos[key].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View key={key} style={styles.toDo}>
+              <Text style={styles.todoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null,
+        )}
       </ScrollView>
     </View>
   );
